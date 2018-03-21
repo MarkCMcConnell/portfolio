720e679bb93f8b5d7cb848f1866b7bf1c922076b
korrollir <mcmcconn@gmail.com>
2018-03-20 20:37:09 -0400

    updated project text and links for modals

--- src/data/project-descriptions.js
+++ src/data/project-descriptions.js
@@ -12,27 +12,27 @@ export const projects = [
   },
   {
     title: 'Daycare Log',
-    description: 'description here',
+    description: 'This app was made at the request of a daycare provider in order to go paperless in her daily reporting. The app is designed to be used on a mobile phone according to user request. Future plans is to convert to React Native and include user authentication.',
     image: 'images/daycarelog.jpg',
     alt: 'A screenshot of the Daycare Log app',
-    technologies: 'React, Express, Webpack',
+    technologies: 'React, Node, Webpack',
     liveLink: 'http://daycare-log.herokuapp.com',
     address: 'Daycare Log',
     githubLink: 'https://github.com/korrollir/daycare_log'
   },
   {
     title: 'Portfolio',
-    description: 'description here',
+    description: 'The newest version of my portfolio was created in React. The use of a global SASS-based stylesheet, instead of CSS modules, was decided based upon the smaller number of components.',
     image: 'images/portfolio.jpg',
     alt: 'A screenshot of the Portfolio Page',
     technologies: 'React, SASS, create-react-app',
-    liveLink: 'https://korrollir.github.io',
+    liveLink: 'https://korrollir.github.io/portfolio',
     address: "You're on it!",
-    githubLink: 'https://github.com/korrollir/korrollir.github.io'
+    githubLink: 'https://github.com/korrollir/portfolio'
   },
   {
     title: 'fCC Simon Game',
-    description: 'description here',
+    description: 'The final project of the freeCodeCamp front end curriculum. The primarily challenge was the timing of the sequences to allow for player input. The overall design is based on the old Simon game machine. Strict mode replicates the original design to fail when the player inputs the wrong sequence.',
     image: 'images/simon.jpg',
     alt: 'A screenshot of the Simon Game',
     technologies: 'jQuery, Pug, SASS',
